5c61b3ea0ae8708c246b62076511454937b28433
tsaran <tsaran@student.wsb-nlu.edu.pl>
2025-05-04 18:41:14 +0200

    kosmetyka

--- src/components/navbars/HomeNavbar.jsx
+++ src/components/navbars/HomeNavbar.jsx
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export function HomeNavbar() {
@@ -22,57 +22,16 @@ export function HomeNavbar() {
                 {/* Sekcja nawigacji */}
                 <Navbar.Collapse id="navbar-nav">
                     <Nav className="ml-auto">
-                        {/* Link główny */}
+                        {/* Linki główne */}
                         <Nav.Link as={Link} to="/">Home</Nav.Link>
-
-                        {/* Sekcja Konto */}
-                        <NavDropdown title="Konto" id="konto-dropdown">
-                            <NavDropdown.Item as={Link} to="/logowanie">Logowanie</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/zmianaFirmy">Zmiana firmy</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/informacjeoFirmie">Informacje o firmie</NavDropdown.Item>
-                        </NavDropdown>
-
-                        {/* Sekcja Konfiguracja */}
-                        <NavDropdown title="Konfiguracja" id="konfiguracja-dropdown">
-                            <NavDropdown.Item as={Link} to="/konfiguracja">Konfiguracja</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/odswierzKonfiguracje">Odśwież konfigurację</NavDropdown.Item>
-                        </NavDropdown>
-
-                        {/* Sekcja Funkcje */}
-                        <NavDropdown title="Funkcje" id="funkcje-dropdown">
-                            <NavDropdown.Item as={Link} to="/nowoscWwersji">Nowości w wersji</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/wlaczNoweFunkcje">Włącz nowe funkcje</NavDropdown.Item>
-                        </NavDropdown>
-
-                        {/* Sekcja Pomoc */}
-                        <NavDropdown title="Pomoc" id="pomoc-dropdown">
-                            <NavDropdown.Item as={Link} to="/pomoc">Pomoc</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/pomocJakZaczac">Jak zacząć</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/pomocFilmyInstruktarzowe">Filmy instruktażowe</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/pomocFaq">FAQ</NavDropdown.Item>
-                        </NavDropdown>
-
-                        {/* Sekcja Rejestracja */}
-                        <NavDropdown title="Rejestracja" id="rejestracja-dropdown">
-                            <NavDropdown.Item as={Link} to="/rejestracjaProgramu">Rejestracja programu</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/oprogramie">O programie</NavDropdown.Item>
-                        </NavDropdown>
-
-                        {/* Sekcja Ustawienia */}
-                        <NavDropdown title="Ustawienia" id="ustawienia-dropdown">
-                            <NavDropdown.Item as={Link} to="/ustawieniaOkien">Ustawienia okien</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/ustawieniaOkienImport">Import</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/ustawieniaOkienEksport">Eksport</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/ustawieniaOgolne">Ustawienia ogólne</NavDropdown.Item>
-                        </NavDropdown>
-
-                        {/* Sekcja Widok */}
-                        <NavDropdown title="Widok" id="widok-dropdown">
-                            <NavDropdown.Item as={Link} to="/kompozycjeWidoku">Kompozycje widoku</NavDropdown.Item>
-                        </NavDropdown>
-
-                        {/* Link do zakończenia pracy */}
+                        <Nav.Link as={Link} to="/informacjeoFirmie">Informacje o firmie</Nav.Link>
+                        <Nav.Link as={Link} to="/konfiguracja">Konfiguracja</Nav.Link>
                         <Nav.Link as={Link} to="/koniecPracy">Koniec pracy</Nav.Link>
+                        <Nav.Link as={Link} to="/logowanie">Logowanie</Nav.Link>
+                        <Nav.Link as={Link} to="/nowoscWwersji">Nowości w wersji</Nav.Link>
+                        <Nav.Link as={Link} to="/odswierzKonfiguracje">Odśwież konfigurację</Nav.Link>
+                        <Nav.Link as={Link} to="/wlaczNoweFunkcje">Włącz nowe funkcje</Nav.Link>
+                        <Nav.Link as={Link} to="/zmianaFirmy">Zmiana firmy</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </div>
